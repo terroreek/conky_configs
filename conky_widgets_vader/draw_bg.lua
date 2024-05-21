d0f139376e60d884e6b8d0c49645e4ace01db996
Tareeq Ali <terroreek@chessclub.space>
2024-05-20 23:19:32 -0400

    vader fixes for conky 1.20+

--- conky_widgets_vader/draw_bg.lua
+++ conky_widgets_vader/draw_bg.lua
@@ -18,9 +18,10 @@ corner_r=20
 -- Set the colour and transparency (alpha) of your background.
 
 bg_colour=0x131416
-bg_alpha=0.80
+bg_alpha=0.8
 
 require 'cairo'
+require 'cairo_xlib'
 function rgb_to_r_g_b(colour,alpha)
 	return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
 end
